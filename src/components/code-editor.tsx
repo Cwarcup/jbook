e0f79542b613c61923addd6b9b0b1692c23d2d77
cwarcup <88569957+Cwarcup@users.noreply.github.com>
2022-04-29 16:50:48 -0700

    editor options

--- src/components/code-editor.tsx
+++ src/components/code-editor.tsx
@@ -3,10 +3,19 @@ import MonacoEditor from '@monaco-editor/react';
 const CodeEditor = () => {
   return (
     <MonacoEditor
-      height="50vh"
+      height="45vh"
       language="javascript"
       value="// Write your code here"
       theme="dark"
+      options={{
+        wordWrap: 'on',
+        minimap: { enabled: false },
+        showUnused: false,
+        folding: false,
+        lineNumbersMinChars: 3,
+        fontSize: 16,
+        scrollBeyondLastLine: true,
+      }}
     />
   );
 };
