5809fcc84be066f047af28679280b8cd3204d093
cwarcup <88569957+Cwarcup@users.noreply.github.com>
2022-04-29 16:08:20 -0700

    adding monaco editor

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,10 @@
+// eslint-disable-next-line
 import * as esbuild from 'esbuild-wasm';
 import { useState, useEffect, useRef } from 'react';
 import ReactDOM from 'react-dom';
 import { unpkgPathPlugin } from './plugins/unpkg-path-plugin';
 import { fetchPlugin } from './plugins/fetch-plugins';
+import CodeEditor from './components/code-editor';
 
 const App = () => {
   const ref = useRef<any>();
@@ -73,6 +75,7 @@ const App = () => {
 
   return (
     <div>
+      <CodeEditor />
       <textarea
         value={input}
         onChange={(e) => setInput(e.target.value)}
