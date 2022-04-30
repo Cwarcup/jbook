be5bbf0f30599ed59f2b20e37e505d187953334f
cwarcup <88569957+Cwarcup@users.noreply.github.com>
2022-04-29 17:05:07 -0700

    code editor

--- src/components/code-editor.tsx
+++ src/components/code-editor.tsx
@@ -1,11 +1,22 @@
 import MonacoEditor from '@monaco-editor/react';
 
-const CodeEditor = () => {
+interface CodeEditorProps {
+  initialValue: string;
+  onChange: (value: string) => void;
+}
+const CodeEditor: React.FC<CodeEditorProps> = ({ onChange, initialValue }) => {
+  const onEditorDidMount = (getValue: () => string, monacoEditor: any) => {
+    monacoEditor.onDidChangeModelContent(() => {
+      onChange(getValue());
+    });
+  };
+
   return (
     <MonacoEditor
+      editorDidMount={onEditorDidMount}
+      value={initialValue}
       height="45vh"
       language="javascript"
-      value="// Write your code here"
       theme="dark"
       options={{
         wordWrap: 'on',
@@ -15,6 +26,7 @@ const CodeEditor = () => {
         lineNumbersMinChars: 3,
         fontSize: 16,
         scrollBeyondLastLine: true,
+        automaticLayout: true,
       }}
     />
   );
