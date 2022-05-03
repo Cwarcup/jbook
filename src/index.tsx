36ede1dff64017bbdb1bd4f0e49afab47ec03c0f
cwarcup <88569957+Cwarcup@users.noreply.github.com>
2022-05-03 16:24:56 -0700

    refactor

--- src/index.tsx
+++ src/index.tsx
@@ -1,33 +1,13 @@
 // eslint-disable-next-line
 import 'bulmaswatch/superhero/bulmaswatch.min.css';
-import { useState } from 'react';
 import ReactDOM from 'react-dom';
-import CodeEditor from './components/code-editor';
-import Preview from './components/preview';
-import bundle from './bundler';
+import CodeCell from './components/code-cell';
 
 const App = () => {
-  const [input, setInput] = useState('');
-  const [code, setCode] = useState('');
-
-  // building our bundle
-  const onClick = async () => {
-    const output = await bundle(input);
-    setCode(output);
-  };
-
   return (
-    <div>
-      <CodeEditor
-        initialValue="const a = 1"
-        onChange={(value) => setInput(value)}
-      />
-      <div>
-        <button onClick={onClick}>Submit</button>
-      </div>
-
-      <Preview code={code} />
-    </div>
+    <>
+      <CodeCell />
+    </>
   );
 };
 
