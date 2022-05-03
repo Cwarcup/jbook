3e99d2b9cebe27cfa8eb9be5d8aa1f1ed83a4bd9
cwarcup <88569957+Cwarcup@users.noreply.github.com>
2022-05-03 16:31:06 -0700

    react 18

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,31 @@
 // eslint-disable-next-line
+// import 'bulmaswatch/superhero/bulmaswatch.min.css';
+// import ReactDOM from 'react-dom';
+// import CodeCell from './components/code-cell';
+
+// const App = () => {
+//   return (
+//     <>
+//       <CodeCell />
+//     </>
+//   );
+// };
+
+// ReactDOM.render(<App />, document.getElementById('root'));
+
 import 'bulmaswatch/superhero/bulmaswatch.min.css';
-import ReactDOM from 'react-dom';
 import CodeCell from './components/code-cell';
+import { createRoot } from 'react-dom/client';
 
 const App = () => {
   return (
     <>
       <CodeCell />
+      <CodeCell />
     </>
   );
 };
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const container = document.getElementById('root');
+const root = createRoot(container!); // createRoot(container!) if you use TypeScript
+root.render(<App />);
