92322e79abe30ee09852329d4b08018e6eb47ff7
cwarcup <88569957+Cwarcup@users.noreply.github.com>
2022-05-13 10:39:49 -0700

    proxymiddleware

--- packages/local-api/dist/index.js
+++ packages/local-api/dist/index.js
@@ -1,16 +1,20 @@
-'use strict';
-var __importDefault =
-  (this && this.__importDefault) ||
-  function (mod) {
-    return mod && mod.__esModule ? mod : { default: mod };
-  };
-Object.defineProperty(exports, '__esModule', { value: true });
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
 exports.serve = void 0;
-var express_1 = __importDefault(require('express'));
-var serve = function (port, filename, dir) {
-  var app = express_1.default();
-  return new Promise(function (resolve, reject) {
-    app.listen(port, resolve).on('error', reject);
-  });
+const express_1 = __importDefault(require("express"));
+const http_proxy_middleware_1 = require("http-proxy-middleware");
+const serve = (port, filename, dir) => {
+    const app = (0, express_1.default)();
+    app.use((0, http_proxy_middleware_1.createProxyMiddleware)({
+        target: 'http://localhost:3000',
+        ws: true,
+        logLevel: 'silent',
+    }));
+    return new Promise((resolve, reject) => {
+        app.listen(port, resolve).on('error', reject);
+    });
 };
 exports.serve = serve;
